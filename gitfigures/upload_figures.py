e9554832d6cb8279e6ce3ee983549c420c89cf80
mhtoribio <23717111+mhtoribio@users.noreply.github.com>
2024-09-09 11:42:44 +0200

    fix upload script to handle nested folders

--- gitfigures/upload_figures.py
+++ gitfigures/upload_figures.py
@@ -17,23 +17,28 @@ def find_png_images(directory):
             png_files.append(os.path.join(root, filename))
         for filename in fnmatch.filter(files, '*.jpg'):
             png_files.append(os.path.join(root, filename))
+    png_files = [name.replace("./", "") for name in png_files]
     return png_files
+
+def get_deepest_subdirectories(filenames):
+    deepest_subdirectories = set()
+    for file in filenames:
+        # Get the directory part of the file path
+        dir_path = os.path.dirname(file)
+        if len(dir_path) == 0:
+            continue
+        # Check if the directory has subdirectories
+        while os.path.isdir(dir_path):
+            subdirs = [os.path.join(dir_path, sub) for sub in os.listdir(dir_path) if os.path.isdir(os.path.join(dir_path, sub))]
+            if subdirs:
+                dir_path = max(subdirs, key=lambda s: s.count(os.sep))  # Find the deepest subdirectory
+            else:
+                break
+        deepest_subdirectories.add(dir_path)
+    return list(deepest_subdirectories)
 
 directory = '.'
 png_images = find_png_images(directory)
-png_filenames = list(map(lambda s: s.split("/")[-1], png_images))
-if len(png_filenames) != len(set(png_filenames)): # duplicates
-    print("Duplicate files!")
-    from collections import defaultdict
-    indices = defaultdict(list)
-    for index, item in enumerate(png_filenames):
-        indices[item].append(index)
-    for item, index_list in indices.items():
-        if len(index_list) > 1:
-            for idx in index_list:
-                print(png_images[idx])
-    print("Aborting...")
-    exit(-1)
 
 import pyoverleaf
 api = pyoverleaf.Api()
@@ -42,10 +47,20 @@ projects = api.get_projects()
 project = list(filter(lambda p: p.name == project_name, projects))[0]
 project_id = project.id
 root_folder = api.project_get_files(project_id)
-img_folder = api.project_create_folder(project_id, root_folder.id, base_dir)
+io = pyoverleaf.ProjectIO(api, project_id)
 
+print("Making subdirectories")
+for subdir in get_deepest_subdirectories(png_images):
+    dirname = os.path.join(base_dir, subdir)
+    print(dirname)
+    io.mkdir(dirname, parents=True, exist_ok=True)
+
+# make subdirectories
+io = pyoverleaf.ProjectIO(api, project_id)
+print("Uploading images")
 for img in png_images:
-    print(img)
-    imgname = img.split("/")[-1]
-    with open(img, "rb") as i:
-        api.project_upload_file(project_id, img_folder.id, imgname, i.read())
+    imgname = os.path.join(base_dir, img)
+    print(imgname)
+    with io.open(imgname, "wb+") as f:
+        with open(img, "rb") as i:
+            f.write(i.read())
