7c950fb30df4c5a2c1b5cc77551b910291274a80
JohanASB <jaskja22@student.aau.dk>
2024-10-08 07:10:47 +0200

    changes for antenna script

--- matlab_scripts/antenna_design/patch_antenna_dimensions.m
+++ matlab_scripts/antenna_design/patch_antenna_dimensions.m
@@ -1,15 +1,17 @@
-er = 4.2;
+er = 4.7;
 vo = 3E8;
 fr = 2.44E9;
-h = 1.34e-3;
+h = 1.43e-3;
 mu_0 = 1.26e-6; % permeability of free space in H/m
 epsilon_0 = 8.85e-12; % permittivity of free space in F/m
 
-W = (vo/(2*fr)) * (sqrt(2/er));
+W = (vo/(2*fr)) * (sqrt(2/(er+1)));
 
 eeff = ((er+1)/2) + ((er-1)/2) * (1+(12*(h/W)))^(-1/2);
 
 deltaL = (0.412 * (((eeff+0.3)*(W/h + 0.264))/((eeff-0.258)*(W/h + 0.8)))) * h;
+
+num2sip(deltaL);
 
 L = (1 / (2 * fr * sqrt(eeff) * sqrt(mu_0 * epsilon_0))) - (2 * deltaL);
 
@@ -28,10 +30,9 @@ G1 = (1 / (120 * pi^2)) * integral(integrand, 0, pi);
 
 G12 = (1 / (120 * pi^2)) * integral(integrand2, 0, pi); 
 % Display the result
-% num2sip(G1)
-% num2sip(G12) 
+% num2sip(G1);
+% num2sip(G12);
 
 R = (1)/(2*(G1+G12));
 
 xp = (L/pi)*acos(sqrt(50/R));
-num2sip(xp)
