d12062b6377517c5385c4c03aca8d497cbe13419
Snorre <snorrejohnsen@gmail.com>
2024-10-18 16:56:45 +0200

    updated complex_sample_mul test

--- fpga/cocotb_examples/complex_sample_mul/complex_sample_mul.v
+++ fpga/cocotb_examples/complex_sample_mul/complex_sample_mul.v
@@ -43,7 +43,6 @@ complex_mult(I_x1, I_s1, I_imm_1, Q_x1, Q_s1, Q_imm_1);
 complex_mult(I_x2, I_s2, I_imm_2, Q_x2, Q_s2, Q_imm_2);
 complex_mult(I_x3, I_s3, I_imm_3, Q_x3, Q_s3, Q_imm_3);
 complex_mult(I_x4, I_s4, I_imm_4, Q_x4, Q_s4, Q_imm_4);
-
 end
 
 assign I_y = I_imm_1 + I_imm_2 + I_imm_3 + I_imm_4;
--- fpga/cocotb_examples/complex_sample_mul/test_runner.py
+++ fpga/cocotb_examples/complex_sample_mul/test_runner.py
@@ -8,7 +8,7 @@ tests_dir = os.path.dirname(__file__)
 @pytest.mark.skipif(os.getenv("SIM") in ("ghdl", "nvc"), reason="Verilog not suported")
 # @pytest.mark.parametrize("seed", range(10))
 def test_complex_sample_mul():
-    run(verilog_sources=[os.path.join(tests_dir, "complex_sample_mul.v")], toplevel="complex_sample_mul", module="tb_complex_sample__mul", waves=True)
+    run(verilog_sources=[os.path.join(tests_dir, "complex_sample_mul.v")], toplevel="complex_sample_mul", module="tb_complex_sample_mul", waves=True)
 
 
 if __name__ == "__main__":
