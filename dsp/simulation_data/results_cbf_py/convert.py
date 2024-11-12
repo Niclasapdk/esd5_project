82aef8296de677c70965f84f4c6e22c19e920b97
mhtoribio <23717111+mhtoribio@users.noreply.github.com>
2024-11-12 10:23:47 +0100

    add sim data for ref implementation

--- dsp/simulation_data/results_cbf_py/convert.py
+++ dsp/simulation_data/results_cbf_py/convert.py
@@ -6,3 +6,4 @@ with open("results", "rb") as f:
 slices = [raw[i:i+4*51] for i in range(0, len(raw), 4*51)]
 spectrums = np.asarray([np.frombuffer(s, dtype=np.float32) for s in slices])
 np.save("python_results.npy", spectrums)
+# rename to {snapshot_count}_snapshots.npy naming convention manually
