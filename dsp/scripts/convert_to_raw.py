1868d9fc7038a52c68acaa673c4aaf1ddaf4c612
mhtoribio <23717111+mhtoribio@users.noreply.github.com>
2024-11-15 10:38:05 +0100

    convert data

--- dsp/scripts/convert_to_raw.py
+++ dsp/scripts/convert_to_raw.py
@@ -2,13 +2,16 @@ import scipy.io
 import numpy as np
 import os
 
-files = os.listdir()
+data_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "simulation_data"))
+
+files = os.listdir(data_dir)
 files_to_convert = []
 for f in files:
     if f.endswith(".mat"):
-        files_to_convert.append(f)
+        files_to_convert.append(os.path.abspath(os.path.join(data_dir, f)))
 
 for fname in files_to_convert:
+    print(fname)
     mat = scipy.io.loadmat(fname)
     interleaved_data = mat.get("signal").astype("complex64").flatten()
     max_magnitude = np.abs(interleaved_data).max()
