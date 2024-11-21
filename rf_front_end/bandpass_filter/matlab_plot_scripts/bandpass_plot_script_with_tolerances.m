8e1b273cf9e6d689a66be2fc3aed3a593e4e002e
JohanASB <jaskja22@student.aau.dk>
2024-11-21 12:42:53 +0100

    change more figures ...

--- rf_front_end/bandpass_filter/matlab_plot_scripts/bandpass_plot_script_with_tolerances.m
+++ rf_front_end/bandpass_filter/matlab_plot_scripts/bandpass_plot_script_with_tolerances.m
@@ -1,11 +1,15 @@
+close all
+clear
 A = readtable('bandpass_scaled_tolerances.txt');
 R = rmmissing(A);
-
 R1 = R(1:369,:);
 R2 = R(370:738,:);
 R3 = R(739:1107,:);
 R4 = R(1108:1476,:);
 R5 = R(1477:1845,:);
+
+N = -3+max(R1.Var2);
+[~,closestIndex] = min(abs(N-R1));
 s = semilogx(R1.(1)*1E-9, R1.(2));
 s(1).LineWidth = 1.4;
 hold on
@@ -23,4 +27,11 @@ s4(1).LineWidth = 1.4;
 xlabel('Frequency [GHz]','FontSize',14);
 ylabel('Magnitude [dB]','FontSize',14);
 set(gca,'FontSize',18);
-grid on
+% Define the range
+range = -16:8:16;
+formattedStrings = cell(size(range));
+for i = 1:length(range)
+    formattedStrings{i} = sprintf('%.1f%%', range(i));
+end
+legend(flip(formattedStrings),'Location','northwest')
+title('Tolerance simulation of bandpass filter')
--- test_data/antenna_array/s11_smith_swr/antenna_array_s11_swr_plot_script.m
+++ test_data/antenna_array/s11_smith_swr/antenna_array_s11_swr_plot_script.m
@@ -6,47 +6,68 @@ T2 = readtable('final_s11_2.CSV');
 T3 = readtable('final_s11_3.CSV');
 T4 = readtable('final_s11_4.CSV');
 
-figure
-subplot(2,2,1);
+T1.freq = T1.freq*1e-9;
+T2.freq = T2.freq*1e-9;
+T3.freq = T3.freq*1e-9;
+T4.freq = T4.freq*1e-9;
+
+% Set figure size to 1920x1080 pixels
+FigH = figure('Position', get(0, 'Screensize'));
+
+% Create the tiled layout
+t = tiledlayout(2, 1);
+
+% First plot
+nexttile
 p1 = plot(T1.freq, T1.dB);
 p1.LineWidth = 2;
 p1.Color = '#282a36';
-xlabel('Frequency [GHz]','FontSize',12);
-ylabel('Magnitude [dB]','FontSize',12);
-lgd = legend('Antenna1');
-title('S11')
-lgd.FontSize = 14;
-grid on
-subplot(2,2,2);
+xlabel('Frequency [GHz]', 'FontSize', 12);
+ylabel('Magnitude [dB]', 'FontSize', 12);
+title('S11');
+set(gca, 'FontSize', 18);
+legend('Antenna 1');
+grid on;
+
+% Second plot
+nexttile
 p2 = plot(T2.freq, T2.dB);
 p2.LineWidth = 2;
 p2.Color = '#ff5555';
-xlabel('Frequency [GHz]','FontSize',12);
-ylabel('Magnitude [dB]','FontSize',12);
-lgd = legend('Antenna2');
-title('S11')
-lgd.FontSize = 14;
-grid on
-subplot(2,2,3);
+xlabel('Frequency [GHz]', 'FontSize', 12);
+ylabel('Magnitude [dB]', 'FontSize', 12);
+title('S11');
+set(gca, 'FontSize', 18);
+legend('Antenna 2');
+grid on;
+
+% Export the figure
+exportgraphics(t, 'C:\Users\johba\esd5_project\gitfigures\appendix\antenna_tests\antenna_array_s11_test_1.png', 'Resolution', 300); % 1920x1080 pixels
+
+% Set figure size to 1920x1080 pixels
+FigH = figure('Position', get(0, 'Screensize'));
+q = tiledlayout(2,1);
+nexttile
 p3 = plot(T3.freq, T3.dB);
 p3.LineWidth = 2;
 p3.Color = '#ffb86c'
 xlabel('Frequency [GHz]','FontSize',12);
 ylabel('Magnitude [dB]','FontSize',12);
-lgd = legend('Antenna3');
 title('S11')
-lgd.FontSize = 14;
+set(gca,'FontSize',18)
+legend('Antenna 3');
 grid on
-subplot(2,2,4);
+nexttile
 p4 = plot(T4.freq, T4.dB);
 p4.LineWidth = 2;
 p4.Color = '#6272a4'
 xlabel('Frequency [GHz]','FontSize',12);
 ylabel('Magnitude [dB]','FontSize',12);
-lgd = legend('Antenna4');
 title('S11')
-lgd.FontSize = 14;
+set(gca,'FontSize',18)
+legend('Antenna 4');
 grid on
+exportgraphics(q, 'C:\Users\johba\esd5_project\gitfigures\appendix\antenna_tests\antenna_array_s11_test_2.png', 'Resolution', 300); % 1920x1080 pixels
 %% Plot for SWR test of final antenna
 close all
 clear
