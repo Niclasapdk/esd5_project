df95c6e6fea44d50ba123d6fddc5eb44456f6834
JohanASB <jaskja22@student.aau.dk>
2024-11-25 10:18:31 +0100

    add plots for test section in antenna

--- test_data/antenna_array/s11_smith_swr/antenna_array_s11_swr_plot_script.m
+++ test_data/antenna_array/s11_smith_swr/antenna_array_s11_swr_plot_script.m
@@ -42,7 +42,7 @@ legend('RMSA 2');
 grid on;
 
 % Export the figure
-exportgraphics(t, 'C:\Users\johba\esd5_project\gitfigures\appendix\antenna_tests\antenna_array_s11_test_1.png', 'Resolution', 300); % 1920x1080 pixels
+% exportgraphics(t, 'C:\Users\johba\esd5_project\gitfigures\appendix\antenna_tests\antenna_array_s11_test_1.png', 'Resolution', 300); % 1920x1080 pixels
 
 % Set figure size to 1920x1080 pixels
 FigH = figure('WindowState','fullscreen');
@@ -67,7 +67,7 @@ title('S_{11} parameter')
 set(gca,'FontSize',18)
 legend('RMSA 4');
 grid on
-exportgraphics(q, 'C:\Users\johba\esd5_project\gitfigures\appendix\antenna_tests\antenna_array_s11_test_2.png', 'Resolution', 300); % 1920x1080 pixels
+% exportgraphics(q, 'C:\Users\johba\esd5_project\gitfigures\appendix\antenna_tests\antenna_array_s11_test_2.png', 'Resolution', 300); % 1920x1080 pixels
 %% Plot for SWR test of final antenna
 close all
 clear
@@ -122,7 +122,7 @@ legend('RMSA 2');
 grid on;
 
 % Export the figure
-exportgraphics(t, 'C:\Users\johba\esd5_project\gitfigures\appendix\antenna_tests\antenna_array_s11_test_initial_1.png', 'Resolution', 300); % 1920x1080 pixels
+% exportgraphics(t, 'C:\Users\johba\esd5_project\gitfigures\appendix\antenna_tests\antenna_array_s11_test_initial_1.png', 'Resolution', 300); % 1920x1080 pixels
 
 % Set figure size to 1920x1080 pixels
 FigH = figure('WindowState','fullscreen');
@@ -145,4 +145,68 @@ title('S_{11} parameter')
 set(gca,'FontSize',18)
 legend('RMSA 4');
 grid on
-exportgraphics(q, 'C:\Users\johba\esd5_project\gitfigures\appendix\antenna_tests\antenna_array_s11_test_initial_2.png', 'Resolution', 300); % 1920x1080 pixels
+% exportgraphics(q, 'C:\Users\johba\esd5_project\gitfigures\appendix\antenna_tests\antenna_array_s11_test_initial_2.png', 'Resolution', 300); % 1920x1080 pixels
+%% Final rmsa design plotted against measured data
+close all
+clear
+T1 = readtable('final_s11_1.CSV');
+T2 = readtable('final_s11_2.CSV');
+T3 = readtable('final_s11_3.CSV');
+T4 = readtable('final_s11_4.CSV');
+T5 = readtable('S11_final_antenna_design_2.44.txt');
+
+
+T1.freq = T1.freq*1e-9;
+T2.freq = T2.freq*1e-9;
+T3.freq = T3.freq*1e-9;
+T4.freq = T4.freq*1e-9;
+
+figH = figure('WindowState','fullscreen');
+sgtitle('S_{11} parameter of measured against simulated RMSA','FontSize',22);
+subplot(2,2,1)
+p1 = plot(T1.freq,T1.dB,T5.Var1,T5.Var2,':');
+p1(1).LineWidth = 1.4;
+p1(2).LineWidth = 1.4;
+xlim([2 2.8]);
+ylabel('Magnitude [dB]');
+xlabel('Frequency [GHz]');
+set(gca,'FontSize',18)
+legend('Measured','Simulated','Location','southwest');
+grid on
+
+subplot(2,2,2)
+p2 = plot(T2.freq,T2.dB,T5.Var1,T5.Var2,':');
+p2(1).LineWidth = 1.4;
+p2(2).LineWidth = 1.4;
+xlim([2 2.8]);
+set(gca,'FontSize',18)
+ylabel('Magnitude [dB]');
+xlabel('Frequency [GHz]');
+legend('Measured','Simulated','Location','southwest');
+grid on
+
+subplot(2,2,3)
+p3 = plot(T3.freq,T3.dB,T5.Var1,T5.Var2,':'); 
+xlim([2 2.8]);
+ylabel('Magnitude [dB]');
+xlabel('Frequency [GHz]');
+p3(1).LineWidth = 1.4;
+p3(2).LineWidth = 1.4;
+set(gca,'FontSize',18)
+ylabel('Magnitude [dB]');
+xlabel('Frequency [GHz]');
+legend('Measured','Simulated','Location','southwest');
+grid on
+
+subplot(2,2,4)
+p4 = plot(T4.freq,T4.dB,T5.Var1,T5.Var2,':'); 
+xlim([2 2.8]);
+p4(1).LineWidth = 1.4;
+p4(2).LineWidth = 1.4;
+set(gca,'FontSize',18)
+ylabel('Magnitude [dB]');
+xlabel('Frequency [GHz]');
+legend('Measured','Simulated','Location','southwest');
+grid on
+
+exportgraphics(figH, 'C:\Users\johba\esd5_project\gitfigures\appendix\antenna_tests\antenna_meas_sim_s11.png', 'Resolution', 300); % 1920x1080 pixels
