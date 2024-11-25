944872369c33fa5578f10b6e6afefe7486f85ec3
JohanASB <jaskja22@student.aau.dk>
2024-11-25 13:29:25 +0100

    changes to more figures

--- test_data/antenna_array/s11_smith_swr/antenna_array_s11_swr_plot_script.m
+++ test_data/antenna_array/s11_smith_swr/antenna_array_s11_swr_plot_script.m
@@ -97,55 +97,32 @@ T4.freq = T4.freq*1e-9;
 FigH = figure('WindowState','fullscreen');
 
 % Create the tiled layout
-t = tiledlayout(2, 1);
-
-% First plot
-nexttile
-p1 = plot(T1.freq, T1.dB);
-p1.LineWidth = 2;
-xlabel('Frequency [GHz]', 'FontSize', 12);
-ylabel('Magnitude [dB]', 'FontSize', 12);
-title('S_{11} parameter');
-set(gca, 'FontSize', 18);
-legend('RMSA 1');
-grid on;
-
+t = tiledlayout(3, 1);
+sgtitle('S_{11} parameter of RMSA iterations','FontSize',24);
 % Second plot
 nexttile
 p2 = plot(T2.freq, T2.dB);
 p2.LineWidth = 2;
 xlabel('Frequency [GHz]', 'FontSize', 12);
 ylabel('Magnitude [dB]', 'FontSize', 12);
-title('S_{11} parameter');
 set(gca, 'FontSize', 18);
-legend('RMSA 2');
 grid on;
 
-% Export the figure
-% exportgraphics(t, 'C:\Users\johba\esd5_project\gitfigures\appendix\antenna_tests\antenna_array_s11_test_initial_1.png', 'Resolution', 300); % 1920x1080 pixels
-
-% Set figure size to 1920x1080 pixels
-FigH = figure('WindowState','fullscreen');
-q = tiledlayout(2,1);
 nexttile
 p3 = plot(T3.freq, T3.dB);
 p3.LineWidth = 2;
 xlabel('Frequency [GHz]','FontSize',12);
 ylabel('Magnitude [dB]','FontSize',12);
-title('S_{11} parameter')
 set(gca,'FontSize',18)
-legend('RMSA 3');
 grid on
 nexttile
 p4 = plot(T4.freq, T4.dB);
 p4.LineWidth = 2;
 xlabel('Frequency [GHz]','FontSize',12);
 ylabel('Magnitude [dB]','FontSize',12);
-title('S_{11} parameter')
 set(gca,'FontSize',18)
-legend('RMSA 4');
 grid on
-% exportgraphics(q, 'C:\Users\johba\esd5_project\gitfigures\appendix\antenna_tests\antenna_array_s11_test_initial_2.png', 'Resolution', 300); % 1920x1080 pixels
+% exportgraphics(t, 'C:\Users\johba\esd5_project\gitfigures\appendix\antenna_tests\antenna_array_s11_test_initial_1.png', 'Resolution', 300); % 1920x1080 pixels
 %% Final rmsa design plotted against measured data
 close all
 clear
@@ -162,7 +139,7 @@ T3.freq = T3.freq*1e-9;
 T4.freq = T4.freq*1e-9;
 
 figH = figure('WindowState','fullscreen');
-sgtitle('S_{11} parameter of measured against simulated RMSA','FontSize',22);
+sgtitle('S_{11} parameter of measured and simulated RMSA','FontSize',22);
 subplot(2,2,1)
 p1 = plot(T1.freq,T1.dB,T5.Var1,T5.Var2,':');
 p1(1).LineWidth = 1.4;
@@ -209,4 +186,4 @@ xlabel('Frequency [GHz]');
 legend('Measured','Simulated','Location','southwest');
 grid on
 
-exportgraphics(figH, 'C:\Users\johba\esd5_project\gitfigures\appendix\antenna_tests\antenna_meas_sim_s11.png', 'Resolution', 300); % 1920x1080 pixels
+% exportgraphics(figH, 'C:\Users\johba\esd5_project\gitfigures\appendix\antenna_tests\antenna_meas_sim_s11.png', 'Resolution', 300); % 1920x1080 pixels
