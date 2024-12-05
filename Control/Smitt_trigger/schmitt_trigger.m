cb17cef6d3285f3c9d4a37f35c323375227329d9
Mathias Joergensen <mathiasjoergensen@hotmail.com>
2024-12-05 13:39:08 +0100

    schmitt trigger test

--- Control/Smitt_trigger/schmitt_trigger.m
+++ Control/Smitt_trigger/schmitt_trigger.m
@@ -9,9 +9,9 @@ figure;
 subplot(2,1,1);
 plot(data.Var1,data.Var5, "Color", [0.9,0.9,0]) %Input
 hold on
-plot(data.Var1,data.Var3, "Color", "b")
-title("Sensor Input and Schmitt-Trigger Output")
-legend("Input 1", "Output 1");
+plot(data.Var1,data.Var2, "Color", "r") %Input
+title("Sensor Input")
+legend("Input 1", "Input 2");
 ylabel("Voltage (V)")
 grid on;
 xlim([0.1 0.4])
@@ -19,13 +19,13 @@ ylim([min(data.Var3)-0.2 max(data.Var3)+0.2])
 yticks(0:1:5);
 
 subplot(2,1,2);
-plot(data.Var1,data.Var2, "Color", "r") %Input
+plot(data.Var1,data.Var3, "Color", "b")
 hold on;
 plot(data.Var1,data.Var4, "Color", "g")
 xlabel("Time (s)")
 ylabel("Voltage (V)")
-title("Sensor Input and Schmitt-Trigger Output")
-legend("Input 2", "Output 2")
+title("Schmitt-Trigger Output")
+legend("Output 1", "Output 2")
 grid on;
 xlim([0.1 0.4])
 ylim([min(data.Var4)-0.2 max(data.Var4)+0.2])
